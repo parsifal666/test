a17cd5dff655d8add73f2d5293a2d07907c4df85
Sanjar Ismailov <sanjar2008gmail.com>
2024-12-25 12:53:32 +0500

    h/w

--- uz/najottalim/main.py
+++ uz/najottalim/main.py
@@ -1,53 +1,26 @@
-import requests
-import sqlite3
-import threading
-import json
+import psycopg2
+from AppProperties import *
 
-def save_to_database():
-    url = "https://dummyjson.com/products"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json().get('products', [])
-        conn = sqlite3.connect('products.db')
-        cursor = conn.cursor()
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY,
-            title TEXT,
-            description TEXT,
-            price REAL,
-            brand TEXT,
-            category TEXT
-        )
-        ''')
-        for product in data:
-            cursor.execute('''
-            INSERT OR REPLACE INTO products (id, title, description, price, brand, category)
-            VALUES (?, ?, ?, ?, ?, ?)
-            ''', (
-                product.get('id'),
-                product.get('title'),
-                product.get('description'),
-                product.get('price'),
-                product.get('brand', 'Unknown'),
-                product.get('category', 'Uncategorized')
-            ))
-        conn.commit()
-        conn.close()
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+    )
 
-def save_to_json_file():
-    url = "https://dummyjson.com/products"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        with open('products.json', 'w') as file:
-            json.dump(data, file, indent=4)
+class Person:
+    @staticmethod
+    def get_all_persons():
+        with get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM persons")
+                return cursor.fetchall()
 
-thread1 = threading.Thread(target=save_to_database)
-thread2 = threading.Thread(target=save_to_json_file)
-
-thread1.start()
-thread2.start()
-
-thread1.join()
-thread2.join()
+    @staticmethod
+    def get_one_person(person_id):
+        with get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM persons WHERE id = %s", (person_id,))
+                return cursor.fetchone()
